f5e0f135d87a94e7a684cf21f951acd5a986b36c
rik9228 <rank228130@gmail.com>
2022-05-01 22:24:41 +0900

    Feature：関数分離の実装

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const functions = require("firebase-functions");
+const otherFunctions = require("./otherFunctions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+exports.func1 = otherFunctions.func1;
 
 exports.scheduledFunc = functions.pubsub
   .schedule("*/5 * * * *")
